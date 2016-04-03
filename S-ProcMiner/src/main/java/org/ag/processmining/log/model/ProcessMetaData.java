e60ff70637b2e30eb0495aad8f0186a7a9b2d41c
ahmed <ahmed@DESKTOP-FQ2GOMA>
2016-04-03 02:07:37 +0200

    Adding an activity class

---
+++ S-ProcMiner/src/main/java/org/ag/processmining/log/model/ProcessMetaData.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ag.processmining.log.model;
+
+/**
+ *
+ * @author ahmed
+ */
+public class ProcessMetaData {
+    
+    public final static String CASE_ID_FIELD_NAME= "CASE_ID_FIELD_NAME" ; 
+    public final static String EVENT_CLASS_FIELD_NAME = "EVENT_CLASS_FIELD_NAME" ; 
+    public final static String EVENT_START_TIME_FIELD_NAME = "EVENT_START_TIME_FIELD_NAME" ;  
+    public final static String EVENT_END_TIME_FIELD_NAME = "EVENT_END_TIME_FIELD_NAME" ; 
+    public final static String ORIGINATOR_FIELD_NAME = "ORIGINATOR_FIELD_NAME" ; 
+}
