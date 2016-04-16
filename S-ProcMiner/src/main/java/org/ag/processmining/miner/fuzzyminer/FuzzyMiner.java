aa2b2d07a4ff92aaa796c419b360bbd3600aae79
ahmed <ahmed@DESKTOP-FQ2GOMA>
2016-04-17 01:16:03 +0200

    Adding an activity class

--- S-ProcMiner/src/main/java/org/ag/processmining/miner/fuzzyminer/FuzzyMiner.java
+++ S-ProcMiner/src/main/java/org/ag/processmining/miner/fuzzyminer/FuzzyMiner.java
@@ -1,5 +1,6 @@
-package org.ag.processmining.log.summarizer;
+package org.ag.processmining.miner.fuzzyminer;
 
+import org.ag.processmining.log.summarizer.*;
 import java.util.Map;
 import org.ag.processmining.log.model.AttributeMapping;
 import org.ag.processmining.log.model.CaseId;
@@ -16,7 +17,7 @@ import org.apache.spark.api.java.JavaSparkContext ;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-public class LogSummarizer
+public class FuzzyMiner
 {
   public static void main(String[] args)
   {
@@ -45,45 +46,10 @@ public class LogSummarizer
     // Building Summary data
     JavaPairRDD<CaseId, Event> CASE_ID_EVENT_MAP = RDDSrc.mapToPair(new MapToCaseIdEvent(att_map,event_attributes));
     JavaPairRDD<CaseId, ProcInstance> CASE_ID_PROC_INSTANCE =  CASE_ID_EVENT_MAP.groupByKey().mapToPair(MAP_TO_CASE_ID_PROC_INSTANCE) ;
-     
-    /***************************************************************************
-       **************************************************************************/
-      // Number of process instance
-      
-      long number_process_instances = CASE_ID_PROC_INSTANCE.count();
-      ls.setNumberOfProcessInstances(number_process_instances); 
-      
-      // Number of events
-      long number_events = RDDSrc.count() ;
-      ls.setNumberOfEvents(number_events);
-      // Event classes
-      Map<EventClass, Long> event_class_occurences = CASE_ID_EVENT_MAP.map(EVENT_CLASSES_GETTER).countByValue();
-      ls.setEventClassOccurences(event_class_occurences);
-     
-      // Start event class occurences
-      Map<EventClass, Long> start_event_class_occurences = CASE_ID_PROC_INSTANCE.map(START_EVENT_CLASSES).countByValue();
-      ls.setStartingLogEvents(start_event_class_occurences); 
-      
-      // End event class occurences
-      Map<EventClass, Long> end_event_class_occurences = CASE_ID_PROC_INSTANCE.map(END_EVENT_CLASSES).countByValue();
-      ls.setEndingLogEvents(end_event_class_occurences); 
-     
-      // Originator occurences
-      Map<Originator, Long> event_originator_occurences = CASE_ID_EVENT_MAP.map(EVENT_ORIGINATOR).countByValue();
-      ls.setOriginatorOccurences(event_originator_occurences); 
-       
-      // Originator-EventClass occurences
-      JavaPairRDD<Originator, EventClass> mapToPair = CASE_ID_EVENT_MAP.mapToPair(new PairFunction<Tuple2<CaseId,Event>,Originator,EventClass>(){
-          @Override
-          public Tuple2<Originator, EventClass> call(Tuple2<CaseId, Event> t) throws Exception {
-              return new Tuple2<>(t._2().getOriginator(), t._2().getEventClass()) ;
-          }
-      }); 
-      
-      JavaPairRDD<Originator, Iterable<EventClass>> groupByKey = mapToPair.groupByKey(); 
-      groupByKey.saveAsTextFile("/user/cloudera/test.txt");
-      // Printing the summary
-      ls.print();
+    
+    long count = CASE_ID_PROC_INSTANCE.count(); 
+    System.out.println(count) ;
+    
       
 
   }
