c0e805e6320adce2872c3ed6d136ae3d52e29b8b
ahmed.gater <ahmed.gater@gmail.com>
2016-10-26 21:26:19 +0200

    Time functions like computing duration at the basis of a time unit

---
+++ S-ProcMiner/src/main/java/org/ag/processmining/log/summarizer/utils/TimeUtils.java
@@ -0,0 +1,50 @@
+/*
+ * @author Ahmed Gater (ahmed.gater@gmail.com)
+ */
+package org.ag.processmining.log.summarizer.utils;
+
+import org.joda.time.*;
+
+import java.io.Serializable;
+
+/**
+ * @author ahmed
+ */
+public final class TimeUtils implements Serializable {
+    static final long serialVersionUID = 1L;
+
+    static enum TimeUnit {
+        MONTH,
+        WEEK,
+        DAY,
+        HOUR,
+        MINUTE,
+        SECOND
+    }
+    private TimeUtils() {
+
+    }
+
+    public static int duration(DateTime start, DateTime end, TimeUnit tu) {
+        if (start == null || end == null) {
+            return -1 ;
+        }
+        switch (tu) {
+            case MONTH:
+                return Months.monthsBetween(start,end).getMonths() ;
+            case WEEK:
+                Weeks.weeksBetween(start, end).getWeeks();
+            case DAY:
+                return Days.daysBetween(start,end).getDays() ;
+            case HOUR:
+                return Hours.hoursBetween(start, end).getHours();
+            case MINUTE:
+                return Minutes.minutesBetween(start,end).getMinutes() ;
+            case SECOND:
+                Seconds.secondsBetween(start,end).getSeconds() ;
+            default:
+                return Days.daysBetween(start,end).getDays() ;
+        }
+    }
+
+}
