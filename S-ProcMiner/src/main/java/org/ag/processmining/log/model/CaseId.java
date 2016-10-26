b5c8dd616d22521bc712a6bbadbfc1fb3a36e09a
ahmed.gater <ahmed.gater@gmail.com>
2016-10-26 20:31:07 +0200

    Refactoring CaseId

--- S-ProcMiner/src/main/java/org/ag/processmining/log/model/CaseId.java
+++ S-ProcMiner/src/main/java/org/ag/processmining/log/model/CaseId.java
@@ -1,66 +1,68 @@
 package org.ag.processmining.log.model;
 
+import lombok.*;
+
 import java.io.Serializable;
-import java.util.Map;
 import java.util.TreeMap;
 
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+@Getter
+@Setter
 public class CaseId implements Serializable {
     static final long serialVersionUID = 1L;
-    private TreeMap<String, String> case_id_attributes;
+    private TreeMap<String, String> fields = null;
 
-    public CaseId(Map<String, String> case_id_flds) {
-        this.case_id_attributes = new TreeMap<>(case_id_flds);
+
+    /*
+    Adding a field to case id
+     */
+    public void addField(String name, String value) {
+        if (fields ==null) fields = new TreeMap<>();
+        this.fields.put(name, value);
     }
 
-    public CaseId() {
-        this.case_id_attributes = new TreeMap<>( );
-    }
-
-    public void addAttribute(String name, String value) {
-        this.case_id_attributes.put(name, value);
-    }
-
-
-    @Override
-    public int hashCode() {
-        StringBuilder sb = new StringBuilder( );
-        for (String field : this.getCase_id_fields( ).navigableKeySet( )) {
-            sb = sb.append(this.getCase_id_fields( ).get(field));
-        }
-        return sb.toString( ).hashCode( );
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (!(obj instanceof CaseId)) {
-            return false;
-        }
-
-        for (String field : this.getCase_id_fields( ).navigableKeySet( )) {
-            if (!this.case_id_attributes.get(field).equalsIgnoreCase(((CaseId) obj).case_id_attributes.get(field))) {
-                return false;
-            }
-        }
-        return true;
+    public String getField(String fld){
+        return fields.containsKey(fld) ? fields.get(fld) : null ;
     }
 
     /**
      * @return the case_id_fields
      */
-    public TreeMap<String, String> getCase_id_fields() {
-        return case_id_attributes;
+    public TreeMap<String, String> getFields() {
+        return fields;
     }
 
     @Override
-    public String toString(){
-        String s="" ;
-        for (String str : this.case_id_attributes.keySet()){
-            s = s.concat(this.case_id_attributes.get(str)) ;
+    public int hashCode() {
+        StringBuilder sb = new StringBuilder();
+        for (String field : this.getFields().navigableKeySet()) {
+            sb = sb.append(this.getFields().get(field));
         }
-        return s ;
+        return sb.toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj == null) ||
+                (!(obj instanceof CaseId)) ||
+                (((CaseId) obj).fields.size() != this.fields.size())) {
+            return false;
+        }
+
+        for (String fld : getFields().navigableKeySet())
+            if (!this.getField(fld).equalsIgnoreCase(((CaseId) obj).getField(fld))) return false ;
+        return true;
+    }
+
+
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (String str : this.fields.keySet()) {
+            s = s.concat(this.fields.get(str));
+        }
+        return s;
     }
 }
