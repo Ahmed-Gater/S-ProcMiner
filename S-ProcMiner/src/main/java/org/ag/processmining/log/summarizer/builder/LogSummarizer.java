2cdf50ade517e8538f926269def0aa69f8837cb9
ahmed.gater <ahmed.gater@gmail.com>
2016-10-28 15:33:36 +0200

    Renaming EventClass to AcivityClass to be complient with process mining communauty concepts

--- S-ProcMiner/src/main/java/org/ag/processmining/log/summarizer/builder/LogSummarizer.java
+++ S-ProcMiner/src/main/java/org/ag/processmining/log/summarizer/builder/LogSummarizer.java
@@ -1,6 +1,6 @@
 package org.ag.processmining.log.summarizer.builder;
 
-import org.ag.processmining.log.model.AttributeMapping;
+import org.ag.processmining.log.model.EventSchema;
 import org.ag.processmining.log.summarizer.overview.LogSummary;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -19,14 +19,14 @@ public class LogSummarizer {
         String sourceFile = args[0];
         String attributeMappingFilePath = args[1];
         String[] event_attributes = {"Case ID", "Activity", "Resource", "Start Timestamp", "Complete Timestamp", "Variant", "Role"};
-        AttributeMapping att_map = new AttributeMapping(attributeMappingFilePath);
+        EventSchema att_map = new EventSchema(attributeMappingFilePath);
         System.out.println(att_map);
         String applicationName = "Process Mining using Apache Spark";
         String applicationDesc = "Building statistics about the process";
 
         SparkConf conf = new SparkConf().setAppName(applicationName).setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        LogSummary lss = LogSummary.buildSummary(sc, applicationName, applicationDesc, sourceFile, event_attributes, att_map);
+        LogSummary lss = LogSummary.buildSummary(sc, sourceFile, event_attributes, att_map);
 
     }
 }
