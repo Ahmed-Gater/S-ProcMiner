527a2b57553fd9172ab9bef043e0669250aeb653
ahmed.gater <ahmed.gater@gmail.com>
2016-10-30 00:49:48 +0200

    cosmetic

--- S-ProcMiner/src/main/java/org/ag/processmining/log/model/Event.java
+++ S-ProcMiner/src/main/java/org/ag/processmining/log/model/Event.java
@@ -24,36 +24,36 @@ public class Event implements Serializable {
     private static final long serialVersionUID = 1L;
     private CaseId caseId;
     private ActivityClass activityClass;
-    private DateTime start ;
-    private DateTime end ;
+    private DateTime start;
+    private DateTime end;
     private Originator originator;
     private Map<String, String> data;
-    private String toto ;
+    private String toto;
 
     private Event(EventBuilder builder) {
         this.caseId = builder.caseId;
-        this.activityClass = builder.activityClass ;
-        this.start = builder.start ;
-        this.end = builder.end ;
-        this.originator = builder.originator ;
-        this.data = builder.data ;
+        this.activityClass = builder.activityClass;
+        this.start = builder.start;
+        this.end = builder.end;
+        this.originator = builder.originator;
+        this.data = builder.data;
     }
 
 
     public double duration(TimeUnit tu) {
-        System.out.println(TimeUtils.duration(start,end,tu)) ;
-        return TimeUtils.duration(start,end,tu)  ;
+        System.out.println(TimeUtils.duration(start, end, tu));
+        return TimeUtils.duration(start, end, tu);
     }
 
     public static class EventBuilder {
         private CaseId caseId;
         private ActivityClass activityClass;
         private Originator originator;
-        private DateTime start ;
-        private DateTime end ;
+        private DateTime start;
+        private DateTime end;
         private Map<String, String> data;
 
-        public EventBuilder(String eventAsString, char fieldDelimiter,String[] header) throws IOException {
+        public EventBuilder(String eventAsString, char fieldDelimiter, String[] header) throws IOException {
             data = ((CSVRecord) CSVParser.parse(eventAsString, CSVFormat.DEFAULT.withHeader(header)
                     .withDelimiter(fieldDelimiter))
                     .getRecords()
@@ -61,7 +61,7 @@ public class Event implements Serializable {
                     .toMap();
         }
 
-        public EventBuilder caseId(List<String> caseIdFields){
+        public EventBuilder caseId(List<String> caseIdFields) {
             this.caseId = new CaseId();
             for (String fld : caseIdFields) {
                 caseId.addField(fld, data.get(fld));
@@ -69,32 +69,33 @@ public class Event implements Serializable {
             for (String fld : caseIdFields) {
                 this.data.remove(fld);
             }
-            return this ;
+            return this;
         }
 
-        public EventBuilder activityClass(String activityClassFieldName){
+        public EventBuilder activityClass(String activityClassFieldName) {
             this.activityClass = new ActivityClass(data.get(activityClassFieldName));
-            this.data.remove(activityClassFieldName) ;
-            return this ;
+            this.data.remove(activityClassFieldName);
+            return this;
         }
 
-        public EventBuilder start(String startField){
+        public EventBuilder start(String startField) {
             this.start = DateFormatExtractor.buildDateTime(data.get(startField));
-            this.data.remove(startField) ;
-            return this ;
+            this.data.remove(startField);
+            return this;
         }
 
-        public EventBuilder end(String endField){
+        public EventBuilder end(String endField) {
             this.end = DateFormatExtractor.buildDateTime(data.get(endField));
-            this.data.remove(endField) ;
-            return this ;
+            this.data.remove(endField);
+            return this;
         }
 
-        public EventBuilder originator(String originatorField){
+        public EventBuilder originator(String originatorField) {
             this.originator = new Originator(data.get(originatorField));
-            this.data.remove(originatorField) ;
-            return this ;
+            this.data.remove(originatorField);
+            return this;
         }
+
         public Event build() {
             return new Event(this);
         }
